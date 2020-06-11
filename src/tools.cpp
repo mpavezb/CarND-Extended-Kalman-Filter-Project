02d6a48512ef27e9bbfe511c79a1ffffe9fb0ce3
Matias Pavez <matias.pavez.b@gmail.com>
2020-06-11 20:37:45 +0200

    Implement CalculateRMSE and unit

--- src/tools.cpp
+++ src/tools.cpp
@@ -12,9 +12,30 @@ Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
-  /**
-   * TODO: Calculate the RMSE here.
-   */
+  VectorXd rmse(4);
+  const auto n_estimations = estimations.size();
+  rmse << 0, 0, 0, 0;
+
+  // Check Inputs
+  if (n_estimations == 0) {
+    std::cout << "Invalid estimation vector (size == 0)." << std::endl;
+    return rmse;
+  }
+  if (n_estimations != ground_truth.size()) {
+    std::cout << "Estimation vector size (" << n_estimations
+              << ") != ground truth size (" << ground_truth.size() << ")."
+              << std::endl;
+    return rmse;
+  }
+
+  for (int i = 0; i < n_estimations; ++i) {
+    VectorXd residual = estimations[i] - ground_truth[i];
+    residual = residual.array() * residual.array();
+    rmse += residual;
+  }
+  rmse = rmse / n_estimations;
+  rmse = rmse.array().sqrt();
+  return rmse;
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd &x_state) {
@@ -22,4 +43,6 @@ MatrixXd Tools::CalculateJacobian(const VectorXd &x_state) {
    * TODO:
    * Calculate a Jacobian here.
    */
+  MatrixXd m{};
+  return m;
 }
--- test/tools_spec.cpp
+++ test/tools_spec.cpp
@@ -1,4 +1,38 @@
+#include "../src/tools.h"
 #include "gtest/gtest.h"
-#include "tools.h"
 
-TEST(sample_case, sample) { EXPECT_TRUE(true); }
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+using std::vector;
+
+constexpr float DELTA_E = 0.001;
+
+TEST(tools_test, CalculateRMSE_GivenEstimationAndGroundTruth_ExpectValidRMSE) {
+  vector<VectorXd> estimations;
+  vector<VectorXd> ground_truth;
+
+  // the input list of estimations
+  VectorXd e(4);
+  e << 1, 1, 0.2, 0.1;
+  estimations.push_back(e);
+  e << 2, 2, 0.3, 0.2;
+  estimations.push_back(e);
+  e << 3, 3, 0.4, 0.3;
+  estimations.push_back(e);
+
+  // the corresponding list of ground truth values
+  VectorXd g(4);
+  g << 1.1, 1.1, 0.3, 0.2;
+  ground_truth.push_back(g);
+  g << 2.1, 2.1, 0.4, 0.3;
+  ground_truth.push_back(g);
+  g << 3.1, 3.1, 0.5, 0.4;
+  ground_truth.push_back(g);
+
+  Tools tools{};
+  const auto rmse = tools.CalculateRMSE(estimations, ground_truth);
+  EXPECT_NEAR(rmse[0], 0.1, DELTA_E);
+  EXPECT_NEAR(rmse[1], 0.1, DELTA_E);
+  EXPECT_NEAR(rmse[2], 0.1, DELTA_E);
+  EXPECT_NEAR(rmse[3], 0.1, DELTA_E);
+}
